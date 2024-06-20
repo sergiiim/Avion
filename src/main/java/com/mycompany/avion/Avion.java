ab0168bc981b8456d9fc95db2b7ebfec40adf649
DAW114 <DAW114@A30PC05b.acarballeira.local>
2024-06-20 11:07:36 +0200

    CommitAvion

---
+++ src/main/java/com/mycompany/avion/Avion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.avion;
+
+/**
+ *
+ * @author DAW114
+ */
+public class Avion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
