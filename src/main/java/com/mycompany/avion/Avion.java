d14d13a95ac8361c6919f2d05a7a46d86c3e8125
DAW114 <DAW114@A30PC05b.acarballeira.local>
2024-06-20 11:26:55 +0200

    Segundo Commit

--- src/main/java/com/mycompany/avion/Avion.java
+++ src/main/java/com/mycompany/avion/Avion.java
@@ -12,6 +12,6 @@ package com.mycompany.avion;
 public class Avion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
     }
 }
